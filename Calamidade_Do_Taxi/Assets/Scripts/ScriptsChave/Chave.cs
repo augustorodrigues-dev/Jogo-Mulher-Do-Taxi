7e8cd3d71788d3a504118f7c85d1d94192fdbd15
augustorodrigues-dev <augustop.rodrigues05@gmail.com>
2025-09-07 21:31:48 -0300

    design segunda fase

--- Calamidade_Do_Taxi/Assets/Scripts/ScriptsChave/Chave.cs
+++ Calamidade_Do_Taxi/Assets/Scripts/ScriptsChave/Chave.cs
@@ -1,25 +1,27 @@
+
 using UnityEngine;
 
 public class Chave : MonoBehaviour
 {
-    
+    public KeyCode teclaParaPegar = KeyCode.E; 
+
     private void OnTriggerStay(Collider other)
     {
-        
+       
         if (other.CompareTag("Player"))
         {
             
             InventarioPlayer player = other.GetComponent<InventarioPlayer>();
 
             
-            if (player != null && player.estaAgachado)
+            if (player != null && player.estaAgachado && Input.GetKeyDown(teclaParaPegar)) 
             {
                 
                 player.temChave = true;
 
-                Debug.Log("Você pegou a chave agachado!");
+                Debug.Log("Você pegou a chave agachado pressionando " + teclaParaPegar.ToString() + "!");
 
-                
+               
                 Destroy(gameObject);
             }
         }
--- Calamidade_Do_Taxi/Assets/Scripts/ScriptsPlayer/InventarioPlayer.cs
+++ Calamidade_Do_Taxi/Assets/Scripts/ScriptsPlayer/InventarioPlayer.cs
@@ -3,31 +3,24 @@ using UnityEngine;
 public class InventarioPlayer : MonoBehaviour
 {
     public bool temChave = false;
-    public bool estaAgachado = false; // <-- NOVA VARIÁVEL
+    public bool estaAgachado = false; 
 
-    public KeyCode teclaAgachar = KeyCode.LeftControl; // Tecla para agachar
+    public KeyCode teclaAgachar = KeyCode.LeftControl; 
 
-    // Para o efeito visual de agachar (opcional)
+    
     void Start()
     {
         
     }
 
     void Update()
+{
+    
+    if (Input.GetKeyDown(teclaAgachar))
     {
-        // Verifica se o jogador pressionou a tecla de agachar
-        if (Input.GetKeyDown(teclaAgachar))
-        {
-            estaAgachado = true;
-            // Efeito visual: diminui a altura do jogador no eixo Y
-            
-        }
-        // Verifica se o jogador soltou a tecla de agachar
-        else if (Input.GetKeyUp(teclaAgachar))
-        {
-            estaAgachado = false;
-            // Efeito visual: retorna o jogador à altura original
-            
-        }
+        
+        estaAgachado = !estaAgachado;
+
     }
+}
 }
